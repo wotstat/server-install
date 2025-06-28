9c0373ac0a65318a6bf7a8bbf16d2117604e858f
Andrei <soprachev@mail.ru>
2025-06-28 06:29:44 +0300

    latest-game-version

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,57 @@
 import { Hono } from 'hono'
 import { getLatestMods, getMods } from './mods-loader'
+import { schedule } from "node-cron";
 
 const app = new Hono()
 
+type GameVersion = { version: string, modsFolder: string, actual: string }
+let latestLestaGameVersion: GameVersion | null = null
+let latestWargamingGameVersion: GameVersion | null = null
+
+async function updateVersions() {
+  const response = await fetch('https://db.wotstat.info/?user=public', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: `
+      select region, modsFolderName, gameVersionFull, datetime
+      from WOT.GameVersionsLatest
+      where region in ['EU', 'RU']
+      format JSON
+    `,
+  })
+
+  const { data } = (await response.json()) as {
+    data: {
+      region: string,
+      modsFolderName: string,
+      gameVersionFull: string,
+      datetime: string,
+    }[]
+  }
+
+  const euVersion = data.find(d => d.region === 'EU')
+  const ruVersion = data.find(d => d.region === 'RU')
+
+  if (!data || !data.length || !euVersion || !ruVersion) {
+    console.error('Failed to fetch game versions');
+    return;
+  }
+
+  latestLestaGameVersion = { version: ruVersion.gameVersionFull, modsFolder: ruVersion.modsFolderName, actual: ruVersion.datetime }
+  latestWargamingGameVersion = { version: euVersion.gameVersionFull, modsFolder: euVersion.modsFolderName, actual: euVersion.datetime }
+}
+
+updateVersions()
+schedule('* */1 * * *', async () => {
+  await updateVersions()
+});
 
 app.get('/latest-game-version', c => {
   return c.json({
-    lesta: '0.9.0',
-    wargaming: '0.9.0',
+    lesta: latestLestaGameVersion,
+    wargaming: latestWargamingGameVersion,
   })
 })
 
