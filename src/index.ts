3ee596e50ae5f348376af1c13173a5c791ca77c2
Andrei <soprachev@mail.ru>
2025-07-13 05:54:40 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { Hono } from 'hono'
 import Api from './api'
 import { loadTask } from './mods-loader'
 import { schedule } from "node-cron";
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+app.use(cors())
 app.route('/api', Api)
 
 loadTask()
